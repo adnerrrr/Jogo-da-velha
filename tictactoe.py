d0206c96426524d6ae9284e4a9d450e1393ee9d2
adnerrrr <alglopes@id.uff.br>
2025-07-18 12:17:10 -0300

    Add files via upload
    Otimizado

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,30 @@
+def jogada(play):
+    match play:
+        case 1 if tabuleiro[0][0] == " ":
+            tabuleiro[0][0] = jog
+        case 2 if tabuleiro[0][1] == " ":
+            tabuleiro[0][1] = jog
+        case 3 if tabuleiro[0][2] == " ":
+            tabuleiro[0][2] = jog
+        case 4 if tabuleiro[1][0] == " ":
+            tabuleiro[1][0] = jog
+        case 5 if tabuleiro[1][1] == " ":
+            tabuleiro[1][1] = jog
+        case 6 if tabuleiro[1][2] == " ":
+            tabuleiro[1][2] = jog
+        case 7 if tabuleiro[2][0] == " ":
+            tabuleiro[2][0] = jog
+        case 8 if tabuleiro[2][1] == " ":
+            tabuleiro[2][1] = jog
+        case 9 if tabuleiro[2][2] == " ":
+            tabuleiro[2][2] = jog
+        case _:
+            print("Jogada inválida, tente novamente: ")
+            return jogada(int(input()))
+
 tabuleiro = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+win = False
+
 for linha in tabuleiro:                           
         print(" | ".join(linha))
 
@@ -8,62 +34,35 @@ for i in range(9):
     else:
         jog = "O"
 
-    jogada = input(f"Jogador {jog}, selecione o local que deseja jogar (1-9): ")                #DECIDE O LUGAR DO TABULEIRO
-    while True:
-        if jogada == "1" and tabuleiro[0][0] == " ":
-            tabuleiro[0][0] = jog
-            break
-        elif jogada == "2" and tabuleiro[0][1] == " ":
-            tabuleiro[0][1] = jog
-            break
-        elif jogada == "3" and tabuleiro[0][2] == " ":
-            tabuleiro[0][2] = jog
-            break
-        elif jogada == "4" and tabuleiro[1][0] == " ":
-            tabuleiro[1][0] = jog
-            break
-        elif jogada == "5" and tabuleiro[1][1] == " ":
-            tabuleiro[1][1] = jog
-            break
-        elif jogada == "6" and tabuleiro[1][2] == " ":
-            tabuleiro[1][2] = jog
-            break
-        elif jogada == "7" and tabuleiro[2][0] == " ":
-            tabuleiro[2][0] = jog
-            break
-        elif jogada == "8" and tabuleiro[2][1] == " ":
-            tabuleiro[2][1] = jog
-            break
-        elif jogada == "9" and tabuleiro[2][2] == " ":
-            tabuleiro[2][2] = jog
-            break
-        else:
-            jogada = input("Jogada inválida, tente novamente: ")
+    jogada(int(input("Onde deseja jogar? (1 - 9) ")))
 
     for linha in tabuleiro:                                    #Tabuleiro
         print(" | ".join(linha))
 
-    if tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[0][2] != " ":                     #Vitória
-        print(f"Jogador {tabuleiro[0][0]} ganhou!")
-        break
-    elif tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[1][2] != " ":
-        print(f"Jogador {tabuleiro[1][0]} ganhou!")
-        break
-    elif tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] != " ":
-        print(f"Jogador {tabuleiro[2][0]} ganhou!")
-        break
-    elif tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0] != " ":
-        print(f"Jogador {tabuleiro[0][0]} ganhou!")
-        break
-    elif tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][1] != " ":
-        print(f"Jogador {tabuleiro[0][1]} ganhou!")
-        break
-    elif tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2] != " ":
-        print(f"Jogador {tabuleiro[0][2]} ganhou!")
-        break
-    elif tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != " ":
-        print(f"Jogador {tabuleiro[0][0]} ganhou!")
-        break
-    elif tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] != " ":
-        print(f"Jogador {tabuleiro[0][2]} ganhou!")
+    match tabuleiro:
+        case tabuleiro if (tabuleiro[0] == ["X"] * 3) or (tabuleiro[0] == ["O"] * 3):                     #Vitória
+            print(f"Jogador {tabuleiro[0][0]} ganhou!")
+            win = True
+        case tabuleiro if (tabuleiro[1] == ["X"] * 3) or (tabuleiro[1] == ["O"] * 3):
+            print(f"Jogador {tabuleiro[1][0]} ganhou!")
+            win = True
+        case tabuleiro if (tabuleiro[2] == ["X"] * 3) or (tabuleiro[2] == ["O"] * 3):
+            print(f"Jogador {tabuleiro[2][0]} ganhou!")
+            win = True
+        case tabuleiro if tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0] != " ":
+            print(f"Jogador {tabuleiro[0][0]} ganhou!")
+            win = True
+        case tabuleiro if tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][1] != " ":
+            print(f"Jogador {tabuleiro[0][1]} ganhou!")
+            win = True
+        case tabuleiro if tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2] != " ":
+            print(f"Jogador {tabuleiro[0][2]} ganhou!")
+            win = True
+        case tabuleiro if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != " ":
+            print(f"Jogador {tabuleiro[0][0]} ganhou!")
+            win = True
+        case tabuleiro if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] != " ":
+            print(f"Jogador {tabuleiro[0][2]} ganhou!")
+            win = True
+    if win == True:
         break
